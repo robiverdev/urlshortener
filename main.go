f8432989f34deb7a3af557dbe9e6f9f5000d9742
robiverdev <robi.verdev1@gmail.com>
2025-11-09 14:08:48 +0100

    add route handlers and URL store

--- main.go
+++ main.go
@@ -7,13 +7,25 @@ import (
 	"net/http"
 )
 
+var urlStore = make(map[string]string)
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("POST /shorten", shortenHandler)
+	mux.HandleFunc("GET /{shortCode}", redirectHandler)
 
 	fmt.Print("Starting server on port :4000\n")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("POST shortening")
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET redirect")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
