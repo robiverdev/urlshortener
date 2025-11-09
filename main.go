cedabf85abde762b8edf3c85f8cd5dbdea75aef7
robiverdev <robi.verdev1@gmail.com>
2025-11-09 14:02:19 +0100

    setting up basic HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Working project")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	fmt.Print("Starting server on port :4000\n")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	_, err := io.WriteString(w, "Homepage")
+
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
